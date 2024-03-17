0f98423372306c5fb9ca07d252d8ffb2aff52486
lulcaaaa <75887286+lulcaaaa@users.noreply.github.com>
2024-03-17 16:27:37 +0100

    Animation-walk-idle
    When walking the animation run is played and when not Idle is

--- BrawPixel/scenes/Purple_Man.cs
+++ BrawPixel/scenes/Purple_Man.cs
@@ -1,56 +1,54 @@
 using Godot;
 using System;
 
-public partial class Purplemain : CharacterBody2D
+public partial class Purple_Man : CharacterBody2D
 {
-	private AnimatedSprite2D _animatedSprite;
-
-    public override void _Ready()
-    {
-        _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
-    }
-    
-    
-    public const float Speed = 300.0f;
+	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	private AnimatedSprite2D animatedSprite;
+	public override void _Ready()
+    {
+        animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
 	
-		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
 
-		// Handle Jump.
+		
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
-			_animatedSprite.Play("run");
+			SwitchAnimation("run");
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			_animatedSprite.Stop();
+			SwitchAnimation("Idle");
 		}
-
-
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		if (Input.IsActionJustPressed("jab"))
+        {
+            SwitchAnimation("jab");
+        }
 	}
+	private void SwitchAnimation(string animationName)
+    {
+        animatedSprite.Animation = animationName;
+    }
+
 	
-
-
-
 }
