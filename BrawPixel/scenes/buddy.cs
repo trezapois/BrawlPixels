dbb41e0d00a3e51d6cbf6026c5d467d016c534b2
Trezapois <gabriel.decuyper@epita.fr>
2024-03-23 17:53:33 +0100

    HP and damage started

---
+++ BrawPixel/scenes/buddy.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class buddy : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+	public int HP = 100;
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+	public void handle_hit(int damage, Vector2 knockback)
+	{
+		HP -= damage;
+	}
+}
