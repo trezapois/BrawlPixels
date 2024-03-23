c62f0ac083b5ca23e364e529cb0a2056760ad776
Trezapois <160756067+trezapois@users.noreply.github.com>
2024-03-23 19:25:48 +0100

    Update buddy.cs

--- BrawPixel/scenes/buddy.cs
+++ BrawPixel/scenes/buddy.cs
@@ -6,6 +6,10 @@ public partial class buddy : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	public int HP = 100;
+
+	 [Signal]
+	public delegate void BuddyDiedEventHandler(); // Change delegate name to follow the convention
+ 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
@@ -25,5 +29,11 @@ public partial class buddy : CharacterBody2D
 		HP -= damage;
 		GD.Print("ouch");
 		GD.Print(HP);
+  		if (HP <= 0)
+        	{
+           		HP = 0; // Ensure HP doesn't go negative
+           		Hide(); // Hide the buddy node when HP reaches 0
+            		EmitSignal(nameof(BuddyDied)); // Emit the signal when HP is zero
+        	}
 	}
 }
