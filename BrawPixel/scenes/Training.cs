433a3269877feecd808a5b3cd6332be8affe4417
Trezapois <160756067+trezapois@users.noreply.github.com>
2024-03-23 19:19:01 +0100

    Update Training.cs

--- BrawPixel/scenes/Training.cs
+++ BrawPixel/scenes/Training.cs
@@ -1,18 +1,23 @@
 using Godot;
 using System;
-
 public partial class Training : Node
 {
 	private Control pauseMenu;
+	private CharacterBody2D buddy;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		pauseMenu = GetNode<Control>("Pause");
+		buddy = GetNode<CharacterBody2D>("Buddy");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (buddy != null && !buddy.Visible)
+        {
+            OnBuddyDied();
+        }
 	}
 	public void _on_pause_2_pressed(){
 		GetTree().Paused= true;
@@ -26,6 +31,14 @@ public partial class Training : Node
 	public void _on_exit_pressed()
 	{
 		GetTree().Paused= false;
+		GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
+	}
+ 	private void OnBuddyDied()
+    {
+        // Transition to the "you won" scene
+        GetTree().ChangeSceneToFile("res://scenes/Win.tscn");
+    }
+}
 		GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
 	}
 }
