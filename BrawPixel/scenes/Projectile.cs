f2a7ea659ed18a2cf292d7628338c76d1420d6ab
lulcaaaa <75887286+lulcaaaa@users.noreply.github.com>
2024-06-09 22:14:14 +0200

    Special attack for purple Man and Retro boy
    animated and made collision box just need to be implement and do damage

---
+++ BrawPixel/scenes/Projectile.cs
@@ -0,0 +1,42 @@
+using Godot;
+
+public partial class Projectile : Area2D
+{
+    [Export]
+    public int Speed = 100; // Speed of the projectile
+    [Export]
+    public int Damage = 15; // Damage dealt by the projectile
+
+    private Vector2 _direction = Vector2.Right; // Default direction
+    private AnimatedSprite2D _animatedSprite; // Reference to the AnimatedSprite2D node
+    public override void _Ready()
+    {
+        GD.Print("Projectile is ready");
+        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _animatedSprite.Play(); // Start the animation when the projectile is ready
+        Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+    }
+
+    public override void _Process(double delta)
+    {
+        MoveProjectile((float)delta);
+    }
+
+    private void MoveProjectile(float delta)
+    {
+        Position += _direction * Speed * delta; // Move in the specified direction
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction.Normalized();
+    }
+    private void OnBodyEntered(Node body)
+    {
+        if (body is IDamageable damageable)
+        {
+            damageable.ApplyDamage(Damage);
+            QueueFree(); // Remove the projectile after hitting
+        }
+    }
+}
